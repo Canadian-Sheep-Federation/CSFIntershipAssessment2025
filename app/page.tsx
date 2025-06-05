9cecc8884a12da0c0aeede17240157d274ed5ccd
Rick Lionel Onana <oricklionel@gmail.com>
2025-06-05 17:18:15 -0400

    Build Cat register form

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,61 @@
+"use client"
 import Image from "next/image";
+import {MdPlayArrow} from "react-icons/md";
+import {FaArrowRight, FaCirclePlus} from "react-icons/fa6";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const catImg = "https://cataas.com/cat/rynTlTM9SkeOjlTT/says/Hello?position=center&font=Impact&fontSize=50&fontColor=%23fff&fontBackground=none"
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div className={"flex flex-col gap-4 items-center justify-center h-screen"}>
+            <h1 className={"text-3xl"}>Register a new cat</h1>
+            <form className={"bg-white p-6 rounded-lg shadow-md"} onSubmit={onSubmit}>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                    Choose a cat
+                </label>
+                <div className={"mb-8 flex flex-col gap-2"}>
+                    <img className={"rounded-md"} height={300} width={300}
+                         src={catImg} alt={"Cat photo"}/>
+                    <button
+                        className={"flex items-center gap-2 justify-center rounded-full cursor-pointer bg-blue-200 px-4 py-1"}>See
+                        next <FaArrowRight/></button>
+                </div>
+
+                <div className={"mb-4"}>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                        Name
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        type="text" id="name" placeholder="Enter cat's name"/>
+                </div>
+
+                <div className={"mb-4"}>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="age">
+                        Age
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        type="number" min={0} max={40} id="age" placeholder="Enter cat's age"/>
+                </div>
+
+                <div className={"mb-4"}>
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="weight">
+                        Weight
+                    </label>
+                    <input
+                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        type="number" step={0.1} min={2} max={15} id="weight" placeholder="Weight in kg"/>
+                </div>
+                <button
+                    className={"flex items-center gap-2 justify-center rounded-full cursor-pointer text-white bg-blue-500 px-4 py-1"}>
+                    <FaCirclePlus/> Save cat
+                </button>
+            </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
