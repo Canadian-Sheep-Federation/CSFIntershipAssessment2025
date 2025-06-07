cbac9d3a4ea86afa15ba6cdcfcb7d68b946a00d8
Rick Lionel Onana <oricklionel@gmail.com>
2025-06-07 18:42:38 -0400

    Add endpoint Get all cats

---
+++ app/api/cats/route.ts
@@ -0,0 +1,19 @@
+import { openDb } from "@/data/db";
+
+export async function GET(req: Request) {
+  const db = await openDb();
+
+  if (db) {
+    const cats = await db.all(`SELECT * FROM cats`);
+    db.close();
+    return new Response(JSON.stringify(cats), {
+      status: 200,
+      headers: { "Content-type": "application/json" },
+    });
+  }
+
+  return new Response(JSON.stringify("Cannot connect to database"), {
+    status: 500,
+    headers: { "Content-type": "application/json" },
+  });
+}
