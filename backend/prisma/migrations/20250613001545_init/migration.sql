9000ff7c0346ad7b7942e1e951a80b5e060b7378
nathan <nathancai1031@gmail.com>
2025-06-12 20:17:12 -0400

    updated database

--- backend/prisma/migrations/20250613001545_init/migration.sql
+++ backend/prisma/migrations/20250613001545_init/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "Word" (
     "id" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
+    "word" TEXT NOT NULL,
+    "definition" TEXT NOT NULL,
+    "partOfSpeech" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -20,7 +22,7 @@ CREATE TABLE "Sentence" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Word_text_key" ON "Word"("text");
+CREATE UNIQUE INDEX "Word_word_key" ON "Word"("word");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Sentence_text_key" ON "Sentence"("text");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,65 @@
 import express from "express";
 import 'dotenv/config'
-import {PrismaClient} from "../generated/prisma"
+import {PrismaClient} from "../generated/prisma/index.js"
 
+const prisma = new PrismaClient();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/word", (req, res) => {
+app.get("/word", async (req, res) => {
   const word = req.query.word as string | undefined;
   if (!word) {
     res.status(400).json({ error: "Word query parameter is required" });
   }
 
-  
+  const wordPrisma = await prisma.word.findFirst({
+    where: {word},
+    select: {
+      sentences: {
+        select: {
+          id: true,
+          text: true,
+        }
+      },
+      word: true,
+      definition: true,
+      partOfSpeech: true,
+    }
+  });
 
-  const apiReq = fetch(`https://api.example.com/words/${word}`);
-  res.json({ word });
+  if (wordPrisma) {
+    console.log(wordPrisma);
+    res.json({ result: wordPrisma });
+    return;
+  }
+  const apiReq = await fetch(`https://www.stands4.com/services/v2/syno.php?uid=${process.env.UID}&tokenid=${process.env.TOKEN_ID}&word=${word}&format=json`);
+  const apiData = await apiReq.json();
+
+  if (apiData.error) {
+    res.status(404).json({ error: "Word not found" });
+    return;
+  }
+
+  const externalWordData = apiData.result.result;
+  const wordRes = {
+    word: externalWordData.term,
+    definition: externalWordData.definition,
+    partOfSpeech: externalWordData.partofspeech,
+    sentences: []
+  };
+
+  const newWord = await prisma.word.create({
+    data: {
+      word: wordRes.word,
+      definition: wordRes.definition,
+      partOfSpeech: wordRes.partOfSpeech,
+    }
+  });
+  console.log(wordRes);
+  res.json({ result: wordRes });
 });
 
 app.listen(PORT, () => {
